93d2b2b17c9b3aa81c439fd7d1ce912991b5fdc7
TerenceGrover <terencegrover@gmail.com>
2023-04-10 17:20:14 +0200

    feat : Mantis Added

--- client/src/components/Portrait.tsx
+++ client/src/components/Portrait.tsx
@@ -6,10 +6,10 @@ export default function Portrait(props: { mouse: { x: number, y: number } }) {
   const links = [
     "https://drive.google.com/uc?id=1gG9iJcAPJ4Z5IcqzVxf-Ax-QBSA0aDyf",
     "https://drive.google.com/uc?id=1svp4ASwaY-ytSpviAWm4Dt4twLFxzb2L",
-    "https://drive.google.com/uc?id=1AxwTm-C2hpdeK1-jDWQt9ihcf9hB74rM",
-    "https://drive.google.com/uc?id=1_dFT3G2Y48BDVbC9lCqRFglpas_fN7zn",
-    "https://drive.google.com/uc?id=12iK7_wG_QeXRfnpyWJdtVIGlpxAGuTMP",
+    "https://drive.google.com/uc?id=18ASHV1kI5JEsPGOBdrorK9lRDbtGSWYF",
     "https://drive.google.com/uc?id=17XZFbBlfy59oYyuURCq7AvhKh6vhfLMC",
+    "https://drive.google.com/uc?id=12iK7_wG_QeXRfnpyWJdtVIGlpxAGuTMP",
+    "https://drive.google.com/uc?id=1_dFT3G2Y48BDVbC9lCqRFglpas_fN7zn",
     "https://drive.google.com/uc?id=1XgJKhxmsXQcdRR_jWStjVyKsZKNi17-k",
     "https://drive.google.com/uc?id=1-Dwxj-QPHsPmWR6ac6D5_zzPeG4zq9g7",
     "https://drive.google.com/uc?id=15G1O7_s_aHtJuz_DOopaXwQVzoT3Z6Zu"
@@ -29,13 +29,13 @@ export default function Portrait(props: { mouse: { x: number, y: number } }) {
         </div>
         <div id="portrait-text-container">
           <p id="portrait-text1">
-            I take portraits of beings I care for. Capturing them in either their natural activities, outdoor activities or in the studio.
+            I take portraits of beings I care for. Capturing them in either their natural environment, outdoor activities or in the studio.
           </p>
           <p id="portrait-text2">
-            Animals are quite a big part of my portrait work. From pets to insects to livestock, they have a completely different essence than humans which is refrteshing. Their eyes is generally my focus, much can be seen through them.
+            Animals are quite a big part of my portrait work. From pets to insects to livestock, they have a completely different essence than humans which is refreshing. Their eyes are generally my focus, much can be seen through them.
           </p>
           <p id="portrait-text3">
-            While I have been working for short periods of time with day shoots, i truly believe that a longer exposure to clients is important to capture their core and personality.
+            While I have been working for clients on single day shoots, i truly believe that a longer exposure to the models is important to capture their core and personality.
           </p>
         </div>
       </div>
