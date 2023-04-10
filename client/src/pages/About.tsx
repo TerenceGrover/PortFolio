33ec9cdf7d6255dd6722a2d02685541b0fd7421d
TerenceGrover <terencegrover@gmail.com>
2023-04-10 18:06:46 +0200

    feat : placeholder About Me page

---
+++ client/src/pages/About.tsx
@@ -0,0 +1,35 @@
+export default function About() {
+  return (
+    <div className='about-container'>
+      <div id='about-bg'></div>
+      <div className='about'>
+        <div className='about-info'>
+          <div className='about-info-text'>
+            <h1>ABOUT</h1>
+            <p>
+              I am a self-taught web developer and designer. I am passionate about creating
+              beautiful and functional websites. I am also a coffee lover and a watch enthusiast.
+            </p>
+          </div>
+          <div className='about-info-text'>
+            <h1>SKILLS</h1>
+            <p>
+              I have a strong background in web development and design. I am proficient in
+              HTML, CSS, JavaScript, React, Express, Mongo, Node.js and Python.
+            </p>
+          </div>
+          <div className='about-info-text'>
+            <h1>CONTACT</h1>
+            <p>
+              I am currently looking for a full-time position as a web developer.
+              If you are interested in working with me, please contact me
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="profile-picture">
+        <img src="https://drive.google.com/uc?id=1eMapF738u7HMkFtitKOnMqslAhIJQLDB" alt="Profile" />
+      </div>
+    </div>
+  );
+}
