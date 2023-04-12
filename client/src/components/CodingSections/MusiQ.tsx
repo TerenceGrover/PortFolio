8fd5a390db5dffed7d0dbf730f17f062de86f134
TerenceGrover <terencegrover@gmail.com>
2023-04-12 16:11:28 +0200

    feat : Queue divs MusiQ

---
+++ client/src/components/CodingSections/MusiQ.tsx
@@ -0,0 +1,45 @@
+import PhotoPlusText from '../PhotoPlusText'
+import Queue from '../Queue'
+import '../styles/musiq.css'
+
+export function MusiQ() {
+  return (
+    <div id='musiq'>
+      <div id="musiq-title">
+        <a href="
+              https://github.com/Al366io/MusiQ/
+            " target="_blank" rel="noreferrer">
+          MusiQ
+        </a>
+      </div>
+      <div id="catch-phrase-musiq">
+        <span id='musiq-catch-1'>
+          Tired of fighting for the AUX or Bluetooth?
+        </span>
+        <span id='musiq-catch-2'>
+          MusiQ solves this issue straight in Spotify.
+        </span>
+      </div>
+      <div id="queue-container">
+        <Queue />
+      </div>
+      <div id="musiq-image-container">
+        <img src="./assets/musiq_user_view.png" alt="MusiQ User View" id="musiq-user-view" />
+        <div id="musiq-shadow"></div>
+      </div>
+      <PhotoPlusText LeftOrRight='right' width='300px' percentage1='30%' percentage2='50%' photo='./assets/player_musiq.png' text={'MusiQ allows users to create a room and invite friends to join. Users can then add songs to the queue and vote on songs to be played next. The host can skip songs and remove songs from the queue.\n This is all achieved with Auth0, the Spotify APi and WebSockets! \n Combination of PSQL, Express and React made this possible!'} />
+
+      <div id="musiq-action-call">
+        <span id='musiq-action-call-text'>
+          Check out the code on Github!
+        </span>
+        <button id='musiq-action-call-button'
+          onClick={() => {
+            window.open('https://github.com/Al366io/MusiQ', '_blank')
+          }}>
+          Github
+        </button>
+      </div>
+    </div>
+  )
+}
