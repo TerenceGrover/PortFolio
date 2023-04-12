8713350e09ec9700b823760ed2beb039d5f8d57f
TerenceGrover <terencegrover@gmail.com>
2023-04-12 12:00:23 +0200

    feat : Street Photography polaroid integration

--- client/src/components/Street.tsx
+++ client/src/components/Street.tsx
@@ -1,3 +1,4 @@
+import StreetShot from './StreetShot'
 import './styles/street.css'
 
 export default function Street() {
@@ -9,6 +10,9 @@ export default function Street() {
         </span>
       </div>
       <div id="street-map">
+        <div id="street-shots-container">
+          <StreetShot url="https://drive.google.com/uc?id=15G1O7_s_aHtJuz_DOopaXwQVzoT3Z6Zu" />
+        </div>
         <img id='street-asset' src="./assets/tokyo_map_display.jpg" alt="" />
         <span id="street-map-text">
           Tokyo, Japan
---
+++ client/src/components/StreetShot.tsx
@@ -0,0 +1,18 @@
+import './styles/street-shot.css'
+
+export default function StreetShot(props: { url: string}) {
+
+  const randomTopLeftPercentage = () => {
+    const randomTop = Math.floor(Math.random() * 100) + '%'
+    const randomLeft = Math.floor(Math.random() * 100) + '%'
+    return { top: randomTop, left: randomLeft }
+  }
+
+  return (
+    <div id="street-shot" style={{
+      ...randomTopLeftPercentage()
+    }}>
+      <img className="polaroid street-img" src={props.url} alt="Street shot" />
+    </div>
+  )
+}
