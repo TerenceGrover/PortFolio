1e64369e2e368e6c9174b18daadbe28fbb321773
TerenceGrover <terencegrover@gmail.com>
2023-04-19 10:29:19 +0200

    optimization : ref & intersectionobserver Polaroid

--- client/src/components/Polaroid.tsx
+++ client/src/components/Polaroid.tsx
@@ -1,5 +1,5 @@
 import './styles/polaroid.css';
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext, useEffect, useRef } from 'react';
 import MouseContext from '../contexts/MouseContext';
 
 export default function Polaroid(props: {
@@ -9,7 +9,20 @@ export default function Polaroid(props: {
   const [shadow, setShadow] = useState('0 4px 6px rgba(0, 0, 0, 0.1)');
   const [glazeStyle, setGlazeStyle] = useState({});
   const [date, setDate] = useState('');
+  const [isInView, setIsInView] = useState(false);
   const { mouse } = useContext(MouseContext);
+
+  const componentRef = useRef(null);
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setIsInView(true);
+      } else {
+        setIsInView(false);
+      }
+    });
+  });
 
   const polaroidStyle = {
     paddingBottom: `${10 / props.aspectRatio}%`,
@@ -19,21 +32,27 @@ export default function Polaroid(props: {
     const min = new Date(2018, 0, 1).getTime();
     const max = new Date().getTime();
     const randomDate = new Date(min + Math.random() * (max - min));
-    return `${randomDate.getFullYear()}-${
-      randomDate.getMonth() + 1
-    }-${randomDate.getDate()}`;
+    return `${randomDate.getFullYear()}-${randomDate.getMonth() + 1
+      }-${randomDate.getDate()}`;
   };
-
-  useEffect(() => {
-    const dx = (-mouse.x / 10) * 0.1;
-    const dy = (-mouse.y / 10) * 0.1;
-
-    setShadow(`${dx}px ${dy}px 7px 3px rgba(0, 0, 0, 0.25)`);
-  }, [mouse]);
 
   useEffect(() => {
     setDate(getRandomDate());
   }, []);
+
+  useEffect(() => {
+    if (!isInView) return;
+    const dx = (-mouse.x / 10) * 0.1;
+    const dy = (-mouse.y / 10) * 0.1;
+    setShadow(`${dx}px ${dy}px 7px 3px rgba(0, 0, 0, 0.25)`);
+  }, [mouse]);
+
+  useEffect(() => {
+    if (componentRef.current) observer.observe(componentRef.current);
+    return () => {
+      if (componentRef.current) observer.unobserve(componentRef.current);
+    };
+  }, [observer, componentRef]);
 
   const handleMouseOver = (e: any) => {
     if (e.timeStamp - e.currentTarget.dataset.lastOver < 75) {
@@ -46,6 +65,7 @@ export default function Polaroid(props: {
     setGlazeStyle({
       background: `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, 0.2) 20%, rgba(255, 255, 255, 0.1) 75%, rgba(255, 255, 255, 0) 100%)`,
     });
+
     // Make the image tilt a bit, depending on the mouse position
     const dx = (e.clientX - window.innerWidth / 2) / 100;
     const dy = (e.clientY - window.innerHeight / 2) / 100;
@@ -53,7 +73,6 @@ export default function Polaroid(props: {
   };
 
   const handleMouseLeave = (e: any) => {
-    setShadow('0 4px 7px 3px rgba(0, 0, 0, 0.3)');
     setGlazeStyle({
       background:
         'linear-gradient(to bottom, rgba(255, 255, 255, 0.3) 10%,  rgba(255, 255, 255, 0.2) 30%, rgba(125, 125, 125, 0.2) 80%, rgba(255, 255, 255, 0.1) 100%)',
@@ -68,6 +87,7 @@ export default function Polaroid(props: {
       style={{ ...polaroidStyle, boxShadow: shadow }}
       onMouseMove={(e) => handleMouseOver(e)}
       onMouseLeave={(e) => handleMouseLeave(e)}
+      ref={componentRef}
     >
       <div
         className="image-wrapper"
